65ce2318265f2751fd2e9a1dce62c759f31e5c27
Max Pegram <maxpeg850@gmail.com>
2024-09-18 14:12:18 -0400

    finished

--- homework3/app.js
+++ homework3/app.js
@@ -1,17 +1,23 @@
 // Homework javascript
-// function calcInterest() {
-//   const principle = document.getElementById("principle").value;
-//   const rate = document.getElementById("rate").value;
-//   const time = document.getElementById("time").value;
-//   console.log("principle", principle);
-//   console.log("rate", rate);
-//   console.log("time", time);
-//   // document.querySelector("#grandTotal").innerHTML =
-//   //   "With a beginning principle of " +
-//   //   principle +
-//   //   " and with a growth rate of $" +
-//   //   rate +
-//   //   "for " +
-//   //   time +
-//   //   "% years, your total interest will be $";
-// }
+function calcInterest() {
+  const principle = parseFloat(document.getElementById("principle").value);
+  const rate = parseFloat(document.getElementById("rate").value);
+  const time = parseFloat(document.getElementById("time").value);
+  const interestTotal = (principle * (rate / 100) * time).toFixed(2);
+  const totalResult = principle + interestTotal;
+  console.log("principle", principle);
+  console.log("rate", rate);
+  console.log("time", time);
+  document.querySelector("#grandTotal").innerHTML =
+    "With a beginning principle of $" +
+    principle +
+    " and with a growth rate of " +
+    rate +
+    "% for " +
+    time +
+    " years, your total interest will be $" +
+    interestTotal +
+    " with a grand total of $" +
+    totalResult +
+    ".";
+}
