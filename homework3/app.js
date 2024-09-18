e94555ce4e76227aff436147333cf7c442b007b4
Max Pegram <maxpeg850@gmail.com>
2024-09-18 12:59:23 -0400

    math

--- homework3/app.js
+++ homework3/app.js
@@ -1,7 +1,13 @@
 // Homework javascript
 function calcInterest(principle, rate, time) {
-  const interest = principle(1 + rate * time);
-  document.querySelector("#grandTotal").innerHTML = interest;
+  const principle = document.getElementById("principle").value;
+  const rate = document.getElementById("rate").value;
+  const time = document.getElementById("time").value;
+  console.log("principle", principle);
+  console.log("rate", rate);
+  console.log("time", time);
+  document.querySelector("#grandTotal").innerHTML +=
+    "With a beginning principle of " + principle + " and with a growth rate of $" + rate + "for " + time + "% years, your total interest will be $" + ;
 }
 
 // In Class work
