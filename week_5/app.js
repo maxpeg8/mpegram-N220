39a7cbb5e65c74679be56fca806b5b842d30a3f0
Max Pegram <maxpeg850@gmail.com>
2024-09-24 17:36:02 -0400

    rest of in class work on bo_objects html

--- week_5/app.js
+++ week_5/app.js
@@ -29,10 +29,31 @@ console.log(
 var tacocat = "wednesday";
 function IUPUIWebsite() {
   console.log(window.tacocat);
-  console.log(`Inner Height: ${window.innerHeight}`);
+  // console.log(`Inner Height: ${window.innerHeight}`);
   console.log(`Inner Width: ${window.innerWidth}`);
-  console.log(`Outer Height: ${window.outerHeight}`);
+  // console.log(`Outer Height: ${window.outerHeight}`);
   console.log(`Outer Width: ${window.outerWidth}`);
-  console.log(window.location);
+  // console.log(window.location);
 }
 IUPUIWebsite();
+
+// Window/Browser Object
+function printPage() {
+  window.print();
+}
+
+//Document Object
+document.getElementById("dpDetails").innerHTML += codeChallenge.courseTitle;
+document.querySelector("#domain").innerHTML += codeChallenge.courseNumber;
+
+const updatePTag = document.getElementById("update");
+console.log(updatePTag.querySelector("#delete"));
+
+console.log(String(100), 100);
+console.log(String(100).length, 100);
+console.log(String(100).includes("0"), 100);
+console.log(String(100).replace("0", "2"), 100);
+console.log(String(100).replaceAll("0", "2"), 100);
+console.log("1, 2, 3, 4".split("#"));
+
+// String Object
