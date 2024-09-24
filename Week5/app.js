3571c4824b39b063c51fb976bb01aad92ece6577
Max Pegram <maxpeg850@gmail.com>
2024-09-24 17:11:22 -0400

    starting in class built in objects

--- Week5/app.js
+++ Week5/app.js
@@ -1,21 +1,38 @@
 // BO_Objects
 
 // Coding Challenge
-const courseNumber = "N220";
-const courseTitle = "Introduction to Media Application Development";
-let totalSeats = 20;
-let avSeats = 18;
+const codeChallenge = {
+  courseNumber: "N220",
+  courseTitle: "Introduction to Media Application Development",
+  totalSeats: 20,
+  enrolledSeats: 18,
+  getAvailibility: function () {
+    return this.totalSeats > this.enrolledSeats;
+  },
+};
 
-function available() {
-  const openSeats = totalSeats - avSeats;
-  console.log(courseNumber + " Open: " + (openSeats > 0));
-}
-available();
+// 1. Are seats available
+console.log(
+  `${codeChallenge.courseNumber} Open: ${codeChallenge.getAvailibility()}`
+);
 
-function available2() {
-  avSeats += 2;
-  console.log("Enrolled: " + avSeats);
-  const openSeats = totalSeats - avSeats;
-  console.log(courseNumber + " Open: " + (openSeats > 0));
+// 2. Two more students enrolled
+codeChallenge.enrolledSeats += 2;
+console.log(`Total Students: ${codeChallenge.enrolledSeats}`);
+
+// 3. Check available again
+console.log(
+  `${codeChallenge.courseNumber} Open: ${codeChallenge.getAvailibility()}`
+);
+
+// BUILT-IN OBJECTS
+var tacocat = "wednesday";
+function IUPUIWebsite() {
+  console.log(window.tacocat);
+  console.log(`Inner Height: ${window.innerHeight}`);
+  console.log(`Inner Width: ${window.innerWidth}`);
+  console.log(`Outer Height: ${window.outerHeight}`);
+  console.log(`Outer Width: ${window.outerWidth}`);
+  console.log(window.location);
 }
-available2();
+IUPUIWebsite();
