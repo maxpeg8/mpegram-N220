548a4d4a3d81945d26aac3b8477700680901e652
Max Pegram <maxpeg850@gmail.com>
2024-10-08 16:54:38 -0400

    finished the midterm work

--- midterm/todo.js
+++ midterm/todo.js
@@ -1,5 +1,30 @@
-const list = document.getElementById("item");
+let itemList = {
+  items: [],
+  name: "Todo List",
+  showList() {
+    document.getElementById("list").innerHTML = "";
 
-function (todoList) {
-  listPTag.innerHTML += `${list} <br />`;
+    this.items.forEach(function (item) {
+      document.getElementById("list").innerHTML += "<li>" + item + "</li>";
+    });
+  },
+};
+
+function addItem() {
+  itemList.items.push(document.getElementById("item").value);
+  itemList.showList();
+  //   console.log(itemList.showList.itemName);
+}
+
+const summaryPTag = document.getElementById("summary");
+
+function summarizeList() {
+  summaryPTag.innerHTML = `1. Total Items: 
+  <li>${itemList.items.length}</li> 
+  <br /> 
+  2. First Item: 
+  <li>${itemList.items[0]}</li>
+  <br />
+  3. Last Item: 
+  <li>${itemList.items[itemList.items.length - 1]}</li>`;
 }
--- week_6/classroom.js
+++ week_6/classroom.js
@@ -4,23 +4,25 @@ const classroom = {
   showStudents() {
     document.getElementById("students").innerHTML = "";
 
-    // this.students.forEach(function (studentName) {
-    //   document.getElementById("students").innerHTML += studentName + "<br />";
-    // });
-    for (let i = 0; i < classroom.students.length; i++) {
-      const studentName = classroom.students[i];
+    this.students.forEach(function (studentName) {
       document.getElementById("students").innerHTML += studentName + "<br />";
-    }
+    });
+    // for (let i = 0; i < classroom.students.length; i++) {
+    //   const studentName = classroom.students[i];
+    //   document.getElementById("students").innerHTML += studentName + "<br />";
+    // }
   },
-  maxStudents: 2,
+  // maxStudents: 2,
 };
 
 function addStudent() {
-  const currentStudents = classroom.students.length;
-  if (currentStudents >= classroom.maxStudents) {
-    console.log("No seats available");
-  } else {
-    classroom.students.push(document.getElementById("studentName").value);
-    classroom.showStudents();
-  }
+  // const currentStudents = classroom.students.length;
+  // if (currentStudents >= classroom.maxStudents) {
+  //   console.log("No seats available");
+  // } else {
+  //   classroom.students.push(document.getElementById("studentName").value);
+  //   classroom.showStudents();
+  // }
+  classroom.students.push(document.getElementById("studentName").value);
+  classroom.showStudents();
 }
