d1c5d0e4e9fbd1aaabfd2b6d162c6f649210b3d7
Max Pegram <maxpeg850@gmail.com>
2024-10-15 17:39:28 -0400

    week 8 in class work

---
+++ week_8/README.md
@@ -0,0 +1,15 @@
+#### Name: Max Pegram
+
+#### Date:10/15/2024
+
+#### Week 8 Folder URL: https://github.com/maxpeg8/mpegram-N220/tree/main/week_8
+
+#### Loops Page URL: https://maxpeg8.github.io/mpegram-N220/week_8/loops.html
+
+#### Index Page URL: https://maxpeg8.github.io/mpegram-N220/week_8/index.html
+
+#### Github Link: https://github.com/maxpeg8/mpegram-N220
+
+## N220 Repo - Week 8
+
+This is my Week 8 folder with my Week 8 work
---
+++ week_8/friends.js
@@ -0,0 +1,89 @@
+// Reference variables
+const friendName = document.getElementById("friendName");
+const friendsListRef = document.getElementById("friendsList");
+
+// Variable to track my friends
+const myFriends = [];
+
+// Function for adding friend
+function addFriend() {
+  // Get the name the user inputed
+  const nameValue = friendName.value;
+  // Add friend to friends list array
+  myFriends.push({ name: nameValue, color: "black" });
+  // Add the name to the friends list
+  showFriends();
+  // Clear the input textbox
+  friendName.value = "";
+}
+
+function removeFriend(friendName) {
+  //   // Use variable to track index of friend to remove
+  //   let friendIndex;
+  //   // Loop through current friends to find the friendName
+  //   for (let i = 0; i < myFriends.length; i++) {
+  //     if (myFriends[i].name === friendName) {
+  //       friendIndex = i;
+  //     }
+  //   }
+  // Remove the person from the list
+  myFriends.splice(friendIndex, 1);
+  showFriends();
+}
+
+// Function to loop through myFriends and "reload" it on the page
+function showFriends() {
+  // Clear pages old friends list
+  friendsListRef.innerHTML = "";
+  // Loop through friends
+  myFriends.forEach(function (friend, friendIndex) {
+    // Add the name to the friends list
+    friendsListRef.innerHTML += `<li>
+    <span style="color: ${friend.color}">${friend.name}</span>
+    <button onclick="removeFriend(${friend.name})">X</button>
+    <button onclick="moveFriend(${friend.name}, true)">&#8593;</button>
+    <button onclick="moveFriend(${friendIndex}, false)">&#8595;</button>
+    <input 
+        type="color" 
+        name="friendTextColor${friendIndex}" 
+        id="friendTextColor${friendIndex}"
+        onchange="changeFriendTextColor(${friendIndex}, event.currentTarget)" 
+    />
+    </li>`;
+  });
+}
+
+function changeFriendTextColor(friendIndex, inputRef) {
+  console.log(friendIndex + " text color to " + inputRef.value);
+  const friendName = myFriends[friendIndex].name;
+  myFriends.splice(friendIndex, 1, {
+    name: friendName,
+    color: inputRef.value,
+  });
+}
+
+// Function to clear the list
+function clearList() {
+  myFriends.splice(0, myFriends.length);
+  showFriends();
+}
+
+// Function to move friends rankings
+function moveFriend(friendName, isMovingUp) {
+  let friendIndex;
+  let friendNewIndex;
+  for (let i = 0; i < myFriends.length; i++) {
+    if (myFriends[i].name === friendName) {
+      friendIndex = i;
+      if (isMovingUp) {
+        friendNewIndex = friendIndex - 1;
+      } else {
+        friendNewIndex = friendIndex + 1;
+      }
+    }
+  }
+  //   const friendName
+  myFriends.splice(friendIndex, 1);
+  myFriends.splice(friendNewIndex, 0, { name: friendName, color: "black" });
+  showFriends();
+}
