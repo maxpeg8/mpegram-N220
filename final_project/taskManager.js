0815b42417274c367abe4640b256a1ab9ba7f054
Max Pegram <maxpeg850@gmail.com>
2024-12-12 22:32:51 -0500

    made the password alerts/error messages work properly

--- final_project/taskManager.js
+++ final_project/taskManager.js
@@ -4,46 +4,40 @@ function loginContent() {
   const loginPageRef = document.getElementById("loginPage");
   loginPageRef.innerHTML = `<div>
   <label for="username">Username: </label>
-  <input type="text" name="username" id="username" required />
+  <input type="text" name="username" id="username" />
   </div>
   <div>
   <label for="password">Password: </label>
-  <input type="password" name="password" id="password" required />
+  <input type="password" id="password" name="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number, one uppercase letter, and at least 8 characters" required />
   </div>
   <button onclick="login()">Login</button>`;
 }
 
-// function managerPage() {
-//   const loginPageRef = document.getElementById("loginPage");
-
-//   loginPageRef.innerHTML = `<div>
-//     <label for="username">Username: </label>
-//     <input type="text" name="username" id="username" />
-//     </div>
-//     <div>
-//     <label for="password">Password: </label>
-//     <input type="text" name="password" id="password" />
-//     </div>
-//     <button onclick="login()">Login</button>`;
-// }
-
-// managerPage();
-
-// alert(
-//   "Password must contain at least 8 characters, a capital letter, and a number"
-// );
-
 function login() {
   const usernameInputRef = document.getElementById("username");
-  if (usernameInputRef.length < 1) {
-    alert("You must fill in a Username");
-  }
   const passwordInputRef = document.getElementById("password");
-  if (passwordInputRef.length < 1) {
-    alert("You must fill in a Password");
-  } else if (passwordInputRef.length < 8) {
+  const usernameValue = usernameInputRef.value;
+  if (usernameValue.length < 1) {
+    alert("You must fill in a username");
+  }
+  const passwordValue = passwordInputRef.value;
+  if (passwordValue.length < 1) {
+    alert("You must fill in a password");
+  } else if (passwordValue.length < 8) {
     alert("Password must be at least 8 characters");
   }
+  let uppercase1 = /[A-Z]/g;
+  if (passwordValue.match(uppercase1)) {
+  } else {
+    alert("Password must contain at least 1 uppercase");
+  }
+  let number1 = /[0-9]/g;
+  if (passwordValue.match(number1)) {
+  } else {
+    alert("Password must contain at least 1 number");
+  }
+  console.log(usernameValue);
+  console.log(passwordValue);
 
   //   usernameInputRef.value = "";
   //   passwordInputRef.value = "";
